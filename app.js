57bbde7f433f1e23131053af894012ce462e6b7e
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-01-16 17:10:20 +0100

    post request

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
+
 const mongoose = require('mongoose');
-const app = express();
+
+app.use(express.json());
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+app.post('/api/sauce', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet créé !'
+    });
 });
 
 app.use('/api/sauce', (req, res, next) => {
