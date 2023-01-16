c65beb63670e6a13dbc5dfae7c7db88270ccd927
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-01-16 16:51:26 +0100

    middleware

--- app.js
+++ app.js
@@ -2,24 +2,31 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-app.use('http://localhost:3000/api/sauce', (req, res, next) => {
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/sauce', (req, res, next) => {
     const sauce = [
-        /*      {
+        /* {
                 _id: 'oeihfzeoi',
                 title: 'Mon premier objet',
                 description: 'Les infos de mon premier objet',
                 imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
                 price: 4900,
                 userId: 'qsomihvqios',
-              },
-              {
+            },
+            {
                 _id: 'oeihfzeomoihi',
                 title: 'Mon deuxième objet',
                 description: 'Les infos de mon deuxième objet',
                 imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
                 price: 2900,
                 userId: 'qsomihvqios',
-              },
+            },
         */
     ];
     res.status(200).json(sauce);
