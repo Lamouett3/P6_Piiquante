9c838aec5c63c06e6799d34f4063c4baddec202b
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-01-18 17:43:50 +0100

    correction

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { rootCertificates } = require('tls');
 const router = express.Router();
+
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
