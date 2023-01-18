c367333d19e330d0f0a94840e13de44aecff8ccd
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-01-18 18:54:50 +0100

    correction mongoose crash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const sauceRoutes = require('./routes/route');
+
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://user:Dropdead692@cluster0.ifa8own.mongodb.net/?retryWrites=true&w=majority',
--- routes/route.js
+++ routes/route.js
@@ -3,14 +3,10 @@ const router = express.Router();
 
 const sauceCtrl = require('../controllers/controller');
 
-router.post('/', sauceCtrl.createSauce);
-
-router.put('/:id', sauceCtrl.modifySauce);
-
-router.delete('/:id', sauceCtrl.deleteSauce);
-
-router.get('/:id', sauceCtrl.getOneSauce);
-
 router.get('/', sauceCtrl.getAllSauce);
+router.post('/', sauceCtrl.createSauce);
+router.get('/:id', sauceCtrl.getOneSauce);
+router.put('/:id', sauceCtrl.modifySauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
 
 module.exports = router;
