86c7619f51ca9230d19aa0e7a52c4b396f9ce551
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-01-18 19:32:58 +0100

    authentification middleware

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const auth = require('../middleware/auth');
+
 const router = express.Router();
 
 const sauceCtrl = require('../controllers/controller');
 
-router.get('/', sauceCtrl.getAllSauce);
-router.post('/', sauceCtrl.createSauce);
-router.get('/:id', sauceCtrl.getOneSauce);
-router.put('/:id', sauceCtrl.modifySauce);
-router.delete('/:id', sauceCtrl.deleteSauce);
+router.get('/', auth, sauceCtrl.getAllSauce);
+router.post('/', auth, sauceCtrl.createSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
