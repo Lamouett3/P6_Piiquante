1191c3b290852a21b88b40138894a8b04473c256
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-01-22 22:18:57 +0100

    correction

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,18 +1,59 @@
-const mongoose = require('mongoose');
+// Requires
+const mongoose = require("mongoose");
 
-const modelSauce = mongoose.Schema({
-    userId: { type: String, required: true },
-    name: { type: String, required: true },
-    manufacturer: { type: String, required: true },
-    description: { type: String, required: true },
-    mainPepper: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    heat: { type: Number, required: true },
-    likes: { type: Number, required: true },
-    dislikes: { type: Number, required: true },
-    usersLiked: { type: ["String <userId>"], required: true },
-    usersDisliked: { type: ["String <userId>"], required: true },
-
+// Creating Schema
+const sauceSchema = mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    manufacturer: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    mainPepper: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+    heat: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 10,
+    },
+    likes: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    dislikes: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    usersLiked: {
+        type: [String],
+        required: true,
+        default: [],
+    },
+    usersDisliked: {
+        type: [String],
+        required: true,
+        default: [],
+    },
 });
 
-module.exports = mongoose.model('Sauce', modelSauce);
+// Exporting Sauce Schema
+module.exports = mongoose.model("Sauce", sauceSchema);
