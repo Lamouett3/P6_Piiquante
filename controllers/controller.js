4c9370d484e8287709323e783948e1de345f36be
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-01-22 20:01:46 +0100

    multer road

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,13 +1,19 @@
 const Sauce = require('../models/Sauce');
+const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
-    delete req.body._id;
+    const sauceObject = JSON.parse(req.body.thing);
+    delete sauceObject._id;
+    delete sauceObject._userId;
     const sauce = new Sauce({
-        ...req.body
+        ...sauceObject,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    sauce.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
+
+    thing.save()
+        .then(() => { res.status(201).json({ message: 'Objet enregistré !' }) })
+        .catch(error => { res.status(400).json({ error }) })
 };
 
 exports.modifySauce = (req, res, next) => {
