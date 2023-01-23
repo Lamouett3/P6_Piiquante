0c7212d402cc6033a7ae48dbe4c73d7bba160205
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-01-23 18:52:56 +0100

    try like function

--- routes/route.js
+++ routes/route.js
@@ -9,6 +9,7 @@ const sauceCtrl = require('../controllers/controller');
 router.get('/', auth, sauceCtrl.getAllSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
+//router.post("/:id/like", auth, multer, sauceCtrl.likeFunction);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
