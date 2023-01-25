a060d077a2dead1593f56e4501c3a9f921be1308
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-01-25 23:32:55 +0100

    fix issue

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator');
 // Création du model User pour un stockage dans la base de données
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true }
 });
 
 // uniqueValidator = évite que plusieurs utilisateurs s'inscrivent avec le même mail
