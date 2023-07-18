67ceaef996d95b2af27fbf46039a6f1fe6e99582
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-07-18 11:57:32 +0200

    reorganize files

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const sauceRoutes = require("./routes/route");
+const userRoutes = require("./routes/user");
+
+// Connexion à la base de données avec mongoose
+mongoose
+  .connect(
+    "mongodb+srv://user:Dropdead692@cluster0.ifa8own.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", sauceRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+module.exports = app;
---
+++ backend/routes/route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const sauceCtrl = require("../controllers/controller");
+
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+
+router.get("/", auth, sauceCtrl.getAllSauce);
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.post("/:id/like", auth, multer, sauceCtrl.likeOrDislike);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+
+module.exports = router;
